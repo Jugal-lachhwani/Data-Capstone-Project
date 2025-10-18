0f4b7c7c8fb4175c2d1714542c72b1b60d73e3ec
Jugal-lachhwani <jugallachhwani15@gmail.com>
2025-10-18 21:23:08 +0530

    Made project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a end to end sentiment analysis project which is fully automated using tools like MLFlow, DVC, AWS EKS, Github actions, etc.',
+    author='Jugal Lachhwani',
+    license='MIT',
+)
