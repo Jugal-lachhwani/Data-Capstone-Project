9aa2b836ca68042af662cca827d5477f44456965
Jugal-lachhwani <jugallachhwani15@gmail.com>
2025-11-08 20:00:38 +0530

    2nd CI/CD-1

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+"""Test package initializer to allow running tests with python -m unittest test.module
+
+This file intentionally left minimal: presence makes `test` a package so
+commands like `python -m unittest test.fastAPI_app_test` work.
+"""
+
+__all__ = []
